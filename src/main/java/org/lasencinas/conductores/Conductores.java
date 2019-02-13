d4d245b8b84a4eb6e3eba4baf7ddc29e8ff29e79
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-02-13 14:05:35 +0100

    Class done

--- src/main/java/org/lasencinas/conductores/Conductores.java
+++ src/main/java/org/lasencinas/conductores/Conductores.java
@@ -1,4 +1,77 @@
 package org.lasencinas.conductores;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Conductores {
+
+    /* ---- Properties --- */
+    private String nombre = null;
+    private String modelo = null;
+    private String matricula = null;
+
+    private double valoracionMedia = 0;
+    private List<Double> valoraciones = new ArrayList<>();
+
+    private boolean ocupado = false;
+
+    /* ---- Constructor ---- */
+    public Conductores(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /* ---- Getters ---- */
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getMatricula() {
+        return this.matricula;
+    }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public double getValoracionMedia() {
+        return this.valoracionMedia;
+    }
+
+    public List<Double> getValoraciones() {
+        return this.valoraciones;
+    }
+
+    public boolean isOcupado() {
+        return this.ocupado;
+    }
+
+    /* ---- Setters ---- */
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+
+    public void setValoraciones(double valoracion) {
+        this.valoraciones.add(valoracion);
+    }
+
+    private void calcularValoraciónMedia() {
+        double sumaValoraciones = 0;
+        for (double valoracion : this.valoraciones) {
+            sumaValoraciones += valoracion;
+        }
+        this.valoracionMedia = sumaValoraciones / this.valoraciones.size();
+    }
 }
