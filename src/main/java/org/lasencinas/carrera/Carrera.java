f96e8767550907d06d807bed63507e49de1e6742
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-02-13 12:54:34 +0100

    Half of class done, yet to test

--- src/main/java/org/lasencinas/carrera/Carrera.java
+++ src/main/java/org/lasencinas/carrera/Carrera.java
@@ -1,22 +1,27 @@
 package org.lasencinas.carrera;
 
 import org.lasencinas.conductores.Conductores;
+import org.lasencinas.tarifa.Tarifa;
 
 public class Carrera {
 
     /* ---- Properties ---- */
     private int tiempoEsperado = 0;
-    private int tiempoCarrera = 0;
+    private double tiempoCarrera = 0;
+
     private double costeTotal = 0;
+    private double distancia = 0;
+
     private String tarjetaCredito = null;
+    private String origen = null;
+    private String destino = null;
+
     private Conductores conductor = null;
 
     /* ---- Constructor ---- */
-    public Carrera(int tiempoEsperado, int tiempoCarrera, double costeTotal, Conductores conductor) {
+    public Carrera(int tiempoEsperado, double distancia) {
         this.tiempoEsperado = tiempoEsperado;
-        this.tiempoCarrera = tiempoCarrera;
-        this.costeTotal = costeTotal;
-        this.conductor = conductor;
+        this.distancia = distancia;
     }
 
     /* ---- Getters ---- */
@@ -24,5 +29,31 @@ public class Carrera {
         return this.tarjetaCredito;
     }
 
+    public String getOrigen() {
+        return this.origen;
+    }
 
+    public String getDestino() {
+        return this.destino;
+    }
+
+    public double getDistancia() {
+        return this.distancia;
+    }
+
+    public double getCosteTotal() {
+        return this.costeTotal;
+    }
+
+    public int getTiempoEsperado() {
+        return this.tiempoEsperado;
+    }
+
+    public double getTiempoCarrera() {
+        return this.tiempoCarrera;
+    }
+
+    public double getCosteEsperado() {
+        return Tarifa.getCosteTotalEsperado(this);
+    }
 }
