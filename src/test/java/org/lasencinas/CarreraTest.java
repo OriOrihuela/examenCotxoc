3e9c6fa5ec5bdb7b86ff0d99f8bb62acc22c311f
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-02-13 13:00:16 +0100

    Constructor test case passed

--- src/test/java/org/lasencinas/CarreraTest.java
+++ src/test/java/org/lasencinas/CarreraTest.java
@@ -1,4 +1,23 @@
 package org.lasencinas;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.lasencinas.carrera.Carrera;
+
 public class CarreraTest {
+
+    private Carrera carrera = null;
+
+    @Before
+    public void __init__() {
+        carrera = new Carrera(0, 0);
+    }
+
+    @Test
+    public void constructorTest() {
+        carrera = new Carrera(20, 50);
+        assertEquals(20, carrera.getTiempoEsperado());
+        assertEquals(50, carrera.getDistancia(), 0.01);
+    }
 }
