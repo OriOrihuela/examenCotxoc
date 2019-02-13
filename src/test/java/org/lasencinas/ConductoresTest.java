9d534b1fd54b964773e7231906b333760f131c48
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-02-13 14:04:51 +0100

    Constructor test case passed

--- src/test/java/org/lasencinas/ConductoresTest.java
+++ src/test/java/org/lasencinas/ConductoresTest.java
@@ -1,4 +1,22 @@
 package org.lasencinas;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.lasencinas.conductores.Conductores;
+
 public class ConductoresTest {
+
+    private Conductores conductor = null;
+
+    @Before
+    public void __init__() {
+        conductor = new Conductores("Juan");
+    }
+
+    @Test
+    public void constructorTest() {
+        String nombre = "Juan";
+        assertEquals(nombre, conductor.getNombre());
+    }
 }
