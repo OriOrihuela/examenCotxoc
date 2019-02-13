d01094812976103450a5037dad6770784e5aff44
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-02-13 15:25:28 +0100

    Class done

--- src/main/java/org/lasencinas/conductores/PoolConductores.java
+++ src/main/java/org/lasencinas/conductores/PoolConductores.java
@@ -1,4 +1,34 @@
 package org.lasencinas.conductores;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class PoolConductores {
+
+    /* ---- Properties ---- */
+    private List<Conductores> poolConductores = new ArrayList<>();
+
+    /* ---- Constructor ---- */
+    public PoolConductores(List<Conductores> poolConductores) {
+        this.poolConductores = poolConductores;
+    }
+
+    /* ---- Getters ---- */
+    public List<Conductores> getPoolConductores() {
+        return this.poolConductores;
+    }
+
+    /* ---- Setters ---- */
+    public Conductores asignarConductor() {
+        Collections.shuffle(this.poolConductores);
+        for (Conductores conductor : this.poolConductores) {
+            if (conductor.isOcupado() == false) {
+                conductor.setOcupado(true);
+                return conductor;
+            }
+        }
+        System.out.println("No es posible asignar un conductor");
+        return null;
+    }
 }
