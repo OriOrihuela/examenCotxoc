ba4e59992085bc7de83c8bb05d341dd4947791c8
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-02-13 12:52:19 +0100

    Test cases passed

--- src/test/java/org/lasencinas/TarifaTest.java
+++ src/test/java/org/lasencinas/TarifaTest.java
@@ -1,10 +1,22 @@
 package org.lasencinas;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.lasencinas.tarifa.Tarifa;
+import org.lasencinas.carrera.Carrera;
+
+import javax.smartcardio.TerminalFactory;
 
 public class TarifaTest {
+
+    private Carrera carrera = null;
+
+    @Before
+    public void __init__() {
+        carrera = new Carrera(0, 0);
+    }
 
     @Test
     public void getCosteDistanciaTest() {
@@ -18,5 +30,19 @@ public class TarifaTest {
         double tiempo = 3.54;
         double esperado = 1.239;
         assertEquals(esperado, Tarifa.getCosteTiempo(tiempo), 0.01);
+    }
+
+    @Test
+    public void getCosteTotalEsperadoTest() {
+        Carrera carrera = new Carrera(30, 12.54);
+        double costeTotal = 30 * 0.35 + 12.54 * 1.35;
+        assertEquals(costeTotal, Tarifa.getCosteTotalEsperado(carrera), 0.01);
+    }
+
+    @Test
+    public void getCosteTotalMínimoTest() {
+        Carrera carrera = new Carrera(1,1);
+        double costeMinimo = 5;
+        assertEquals(costeMinimo, Tarifa.getCosteTotalEsperado(carrera), 0.01);
     }
 }
