e5a8a0fdfaf035e6add7aeb2d7391f831856741e
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-02-13 14:34:57 +0100

    valoracionMediaTest passed

--- src/test/java/org/lasencinas/ConductoresTest.java
+++ src/test/java/org/lasencinas/ConductoresTest.java
@@ -5,6 +5,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.lasencinas.conductores.Conductores;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ConductoresTest {
 
     private Conductores conductor = null;
@@ -18,5 +23,15 @@ public class ConductoresTest {
     public void constructorTest() {
         String nombre = "Juan";
         assertEquals(nombre, conductor.getNombre());
+    }
+
+    @Test
+    public void valoracionMediaTest() {
+        List<Double> valoraciones = new ArrayList<>(Arrays.asList(5.65, 4.56, 2.45, 8.99));
+        for (double valoracion : valoraciones){
+            conductor.setValoracion(valoracion);
+        }
+        conductor.setValoracionMedia();
+        assertEquals(5.4125, conductor.getValoracionMedia(), 0.01);
     }
 }
